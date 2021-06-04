f30da6013cd444f5dc17087ab6692e2db647d968
carlosiglesias1 <carlosig490@gmail.com>
2021-06-04 22:05:41 +0200

    cambios

--- mezclaLeerTrocear.c
+++ mezclaLeerTrocear.c
@@ -5,7 +5,7 @@
 
 int abrirDirectorio();
 void procesoArchivo();
-char *trocearCadena();
+int trocearCadena();
 
 int main()
 {
@@ -15,13 +15,16 @@ int main()
 
 int abrirDirectorio()
 {
+    /*Declaro donde voy a guardar el trozo de la cadena*/
+    char trozo[30];
+    int count = 0;
     /* Con un puntero a DIR abriremos el directorio */
     DIR *dir;
     /* en *ent habrá información sobre el archivo que se está "sacando" a cada momento */
     //struct dirent *ent;
-
+    struct dirent *ent;
     /* Empezaremos a leer en el directorio actual */
-    dir = opendir(".");
+    dir = opendir("F:/Fotos/WhatsApp Images");
 
     /* Miramos que no haya error */
     if (dir == NULL)
@@ -29,11 +32,20 @@ int abrirDirectorio()
         perror("No puedo abrir el directorio");
         return -1;
     }
-    procesoArchivo("leerDirectorio.c");
+    while ((ent = readdir(dir)) != NULL && count < 10)
+    {
+        /* Nos devolverá el directorio actual (.) y el anterior (..), como hace ls */
+        if ((strcmp(ent->d_name, ".") != 0) && (strcmp(ent->d_name, "..") != 0))
+        {
+            /* Una vez tenemos el archivo, lo pasamos a una función para procesarlo. */
+            procesoArchivo(ent->d_name, trozo);
+        }
+        count++;
+    }
     closedir(dir);
     return 1;
 }
-void procesoArchivo(char *archivo)
+void procesoArchivo(char *archivo, char trozo[])
 {
     /* Para "procesar", o al menos, hacer algo con el archivo, vamos a decir su tamaño en bytes */
     /* para ello haremos lo que vemos aquí: https://poesiabinaria.net/2010/04/tamano-de-un-fichero-en-c/ */
@@ -47,27 +59,25 @@ void procesoArchivo(char *archivo)
         fclose(fich);
         /* Si todo va bien, decimos el tamaño */
         printf("%30s (%ld bytes)\n", archivo, ftam);
-        printf("%s", trocearCadena(archivo));
+        printf("%30d\n", trocearCadena(archivo, trozo));
+        printf("%30s\n", trozo);
     }
     else
     {
         /* Si ha pasado algo, sólo decimos el nombre */
         printf("%30s (No info.)\n", archivo);
-        printf("%s", trocearCadena(archivo));
+        printf("%30d\n", trocearCadena(archivo, trozo));
+        printf("%30s\n", trozo);
     }
 }
 
-char *trocearCadena(char *archivo)
+int trocearCadena(char *archivo, char trozo[])
 {
     /*Copio el nombre del archivo, ya que no puedo cortarlo directamente*/
-    char archivoCorte[strlen(archivo)];
-    strcpy(archivoCorte, archivo); 
+    strcpy(trozo, archivo);
     /*Establezco un delimitador*/
     char delimitador[] = "D";
     /*Corto la cadena hasta el delimitador*/
-    char *resultado = strtok(archivoCorte, delimitador);
-    /*Despues de cortar, asigno el valor de fin de cadena*/
-    archivoCorte[strlen(archivoCorte)+1] = '\0'; 
-    printf("%s", resultado);
-    return resultado;
+    strtok(trozo, delimitador);
+    return 0;
 }
